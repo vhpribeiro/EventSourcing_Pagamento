6d9e4d922252d988e63d81f636a1d594dee7682c
Vitor Hugo Pereira Ribeiro <vitorhugopereiraribeiro@gmail.com>
2020-10-26 19:19:13 -0400

    Retirando connection string chumbada

--- EventSourcing_Pagamento.API/Startup.cs
+++ EventSourcing_Pagamento.API/Startup.cs
@@ -5,6 +5,7 @@ using EventSourcing_Pagamento.Aplicacao.Pagamentos;
 using EventSourcing_Pagamento.Infra.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,16 +16,18 @@ namespace EventSourcing_Pagamento.API
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            ConfiguracaoDeInjecaoDeDependencia.Configurar(services, Configuration);
+            ConfiguracaoDeInjecaoDeDependencia.Configurar(services, _configuration);
             services.AddControllers();
-            services.AddDbContext<PagamentoContext>();
+            services.AddDbContext<PagamentoContext>(options => 
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("EventSourcing_Pagamento.Infra")));
             services.AddHostedService<RabbitMqSubscriber>();
         }
 
--- EventSourcing_Pagamento.Infra/Contexts/PagamentoContext.cs
+++ EventSourcing_Pagamento.Infra/Contexts/PagamentoContext.cs
@@ -17,13 +17,6 @@ namespace EventSourcing_Pagamento.Infra.Contexts
         }
         
         public DbSet<Pagamento> Pagamentos { get; set; }
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //TODO jogar essa connection string no appsettings
-            optionsBuilder.UseSqlServer("Password=vhpr1706;Persist Security Info=True;User ID=sa;Initial Catalog=EventSourcingPagamento;Data Source=DESKTOP-NEOJFCR\\MSSQLSERVER2019",
-                b => b.MigrationsAssembly("EventSourcing_Pagamento.Infra"));
-        }
 
     }
 }
